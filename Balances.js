f009c75662a00d929b05110dd761b1024391886e
OlegKoleskin <oleg.koleskin@gmail.com>
2018-01-18 23:22:23 +0200

    WIP adding balances

---
+++ Balances.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+  el: "#app",
+
+  data() {
+    return {
+      object: {}
+    };
+  },
+  methods: {
+    saveToFireBase() {
+      var userId = firebase.auth().currentUser.uid;
+      firebase.database().ref(`/${userId}/myCoins`).set(this.object);
+
+    }
+  },
+  mounted() {
+    var credentials = getFbCredentials();
+    if (!firebase.auth().currentUser) {
+      console.log('authentication required');
+      firebase.auth().signInWithEmailAndPassword(credentials[0], credentials[1]).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('FireBase authentication error', errorCode, errorMessage);
+      });
+    }
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) { // User is signed in.
+        console.log('in on auth changed');
+        var userId = firebase.auth().currentUser.uid;
+        return firebase.database().ref(`/${userId}/myCoins`).once('value').then((snapshot) => {
+          this.object = Object.assign(this.object, snapshot.val());
+          //todo: don't should UI until not loaded
+        });
+      }
+    });
+  }
+});
